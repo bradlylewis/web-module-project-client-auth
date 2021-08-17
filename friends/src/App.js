d104ba739f1979acdf497ac000fae49ecd94537f
Bradly Lewis <bradlylewisdev@gmail.com>
2021-08-17 17:53:54 -0400

    add login route to app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+// Components
+import Login from './components/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path='/login' component={Login}/>
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
